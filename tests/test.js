72ac907039297e7d38987d159177491d2140a1fb
chamiru.uuu <chamiru.codes@gmail.com>
2024-09-21 09:46:39 -0700

    for the pages host

---
+++ tests/test.js
@@ -0,0 +1,14 @@
+const textContainer = document.querySelector(".textContainer");
+let easeFactor = 0.02;
+let scene, camera, renderer, planeMesh;
+let mousePosition = { x: 0.5, y: 0.5 };
+let targetMousePosition = { x: 0.5, y: 0.5 };
+let prevPosition = { x: 0.5, y: 0.5 };
+
+const vertexShader = `
+varying vec2 vUv;
+void main() {
+  vUv = uv;
+  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+}
+`;

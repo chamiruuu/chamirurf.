34830bd6b1a4b67d2251762dcfa251b00327f31f
chamiru.uuu <chamiru.codes@gmail.com>
2024-09-22 00:45:54 -0700

    Prepare for deployment

--- src/components/Shader.jsx
+++ src/components/Shader.jsx
@@ -14,7 +14,7 @@ const Shader = () => (
     >
         <ShaderGradient
             control='query'
-            urlString='https://www.shadergradient.co/customize?animate=on&axesHelper=off&bgColor1=%23000000&bgColor2=%23000000&brightness=1.8&cAzimuthAngle=180&cDistance=2.8&cPolarAngle=80&cameraZoom=9.1&color1=%2300706c&color2=%2347b49d&color3=%23212121&destination=onCanvas&embedMode=off&envPreset=city&format=gif&fov=45&frameRate=30&gizmoHelper=hide&grain=on&lightType=3d&pixelDensity=1&positionX=0&positionY=0&positionZ=0&range=disabled&rangeEnd=40&rangeStart=0&reflection=0&rotationX=10&rotationY=0&rotationZ=45&shader=defaults&type=waterPlane&uAmplitude=0&uDensity=1.2&uFrequency=0&uSpeed=0.1&uStrength=4&uTime=8&wireframe=false'
+            urlString='https://www.shadergradient.co/customize?animate=on&axesHelper=off&bgColor1=%23000000&bgColor2=%23000000&brightness=0.6&cAzimuthAngle=180&cDistance=2.8&cPolarAngle=80&cameraZoom=9.1&color1=%2300706c&color2=%2347b49d&color3=%23212121&destination=onCanvas&embedMode=off&envPreset=city&format=gif&fov=45&frameRate=30&gizmoHelper=hide&grain=on&lightType=3d&pixelDensity=1&positionX=0&positionY=0&positionZ=0&range=disabled&rangeEnd=40&rangeStart=0&reflection=0&rotationX=10&rotationY=0&rotationZ=45&shader=defaults&type=waterPlane&uAmplitude=0&uDensity=1.2&uFrequency=0&uSpeed=0.1&uStrength=4&uTime=8&wireframe=false'
         />
     </ShaderGradientCanvas>
 );
